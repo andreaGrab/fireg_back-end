26efef6cdf5e5ceffb0cc420676f6493d0b184d9
Andrea <andrea96grb@gmail.com>
2021-08-01 01:11:42 +0200

    configure redirect routes for front-end

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -14,7 +14,7 @@ exports.login = function(req, res, next){
 				jwt.sign({name:userDb[0].name},process.env.JWT_SECRET, {expiresIn: 30},(err, token)=>{
 					// setting cookies
 					res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000});
-					res.status(200).redirect('/reg');
+					res.status(200).redirect('localhost:3000/reg');
 				});
 			}else{
 				res.status(400).redirect('/ercred');
