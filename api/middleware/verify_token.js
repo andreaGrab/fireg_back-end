15d973322833d6b73c848369eec0d9a8bfd01e27
Andrea <andrea96grb@gmail.com>
2021-08-01 20:30:36 +0200

    variant of authorization

--- api/middleware/verify_token.js
+++ api/middleware/verify_token.js
@@ -1,13 +1,13 @@
 module.exports = (req, res, next)=>{
 		let headerAuth = req.headers.authorization;
-		if(typeof headerAuth !== 'string'){
+		/*if(typeof headerAuth !== 'string'){
 			res.status(400);
 			console.log(headerAuth + "from authorization header");
 		}else{
 			let cookieVal = req.headers.cookie.split("token=");
 			console.log(cookieVal + "from cookie header");
 			headerAuth="Bearer " + cookieVal[1];
-		}
+		}*/
 		if(headerAuth != null){
 			// take from header authorization the token, splitting space between bearer & the token
 			const token = headerAuth.split(" ")[1];
