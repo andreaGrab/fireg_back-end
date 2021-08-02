47df02f63f2ef4fe7351e05c3c01ec3ec7b22d89
Andrea <andrea96grb@gmail.com>
2021-08-03 00:44:31 +0200

    restore CORS

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.use(bodyParser.json({extended:true}));// parsing raw json body
 
 // CORS errors prevention///////////////
 app.use(cors({
+	credentials: true,
 	origin: 'https://localhost:3000',
-	methods: ['GET', 'POST', 'DELETE']
-	//allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+	methods: ['GET', 'POST', 'DELETE'],
+	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
 
 
