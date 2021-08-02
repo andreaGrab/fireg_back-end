64258e9795cf8b9f4c361a98c553242d570805e8
Andrea <andrea96grb@gmail.com>
2021-08-03 00:40:05 +0200

    removed credentials prop CORS'

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ app.use(bodyParser.json({extended:true}));// parsing raw json body
 
 // CORS errors prevention///////////////
 app.use(cors({
-	credentials: true,
 	origin: 'https://localhost:3000',
 	methods: ['GET', 'POST', 'DELETE']
 	//allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
