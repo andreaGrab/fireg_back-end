2b81c6036ebd3a44b49c5ca1223f3a8b88f693ad
Andrea <andrea96grb@gmail.com>
2021-08-02 20:49:33 +0200

    config headers in CORS

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(bodyParser.json({extended:true}));// parsing raw json body
 app.use(cors({
 	credentials: true,
 	origin: true,
-	methods: ['GET', 'POST', 'DELETE'],
-	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+	methods: ['GET', 'POST', 'DELETE']
+	//allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
 
 
