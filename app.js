badebad423ecf1f59a59c3d20025bf5329573753
Andrea <andrea96grb@gmail.com>
2021-08-02 20:30:00 +0200

    CORS no origin

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use(bodyParser.json({extended:true}));// parsing raw json body
 // CORS errors prevention///////////////
 app.use(cors({
 	credentials: true,
-	origin: "*",
 	methods: ['GET', 'POST', 'DELETE'],
 	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
